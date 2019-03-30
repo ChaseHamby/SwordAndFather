e56903fe4f2cc38e51852114400893bee596a6fc
unknown <c.chase.hamby@gmail.com>
2019-03-30 10:41:23 -0500

    adding folders and new Assassin Controller

--- SwordAndFather/Controllers/UsersController.cs
+++ SwordAndFather/Controllers/UsersController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SwordAndFather.Data;
 using SwordAndFather.Models;
 
 namespace SwordAndFather.Controllers
@@ -12,24 +13,29 @@ namespace SwordAndFather.Controllers
     [ApiController] // <<< Attribute - there for the framework to use on how to expose the data
     public class UsersController : ControllerBase // Controller receives an HTTP request and then responds
     {
-        static List<User> _users = new List<User>(); // static means you get one of OR all instances of a class - 
-            // every instance in this class will share the same instance of users
+        readonly UserRepository _userRepository;
+        readonly CreateUserRequestValidator _validator;
 
-        [HttpPost("register")] // this means 'api/user/register' - this is the HTTP route that calls the AddUser method below
-
-        public ActionResult<int> AddUser(User newUser) 
+        public UsersController()
         {
-            //validation
-            if(string.IsNullOrEmpty(newUser.Username)
-                || string.IsNullOrEmpty(newUser.Password))
+            _validator = new CreateUserRequestValidator();
+            _userRepository = new UserRepository();
+        }
+
+        [HttpPost("register")] // this means 'api/user/register': this is the HTTP route that calls the AddUser method below
+
+        public ActionResult<int> AddUser(CreateUserRequest createRequest) 
+        {
+            if(!_validator.Validate(createRequest))
             {
                 return BadRequest(new {error = "users must have a username and password"});
             }
 
-            newUser.Id = _users.Count + 1;
-            _users.Add(newUser);
+            var newUser = _userRepository.AddUser(createRequest.Username, createRequest.Password);
 
+            // http response
             return Created($"api/users/{newUser.Id}", newUser);
         }
     }
+
 }
---
+++ SwordAndFather/Data/UserRepository.cs
@@ -0,0 +1,27 @@
+﻿using SwordAndFather.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SwordAndFather.Data
+{
+    public class UserRepository
+    {
+        static List<User> _users = new List<User>();
+        // static ^^^ means you get one of OR all instances of a class
+        // every instance in this class will share the same instance of users
+
+        public User AddUser(string username, string password)
+        {
+            var newUser = new User(username, password);
+
+            newUser.Id = _users.Count + 1;
+
+            _users.Add(newUser);
+
+            return newUser;
+        }
+
+    }
+}
